c2e21385b41dc574c380a8cc28293b0137e94adb
irirusik97-code <irirusik97@gmail.com>
2025-12-16 00:03:51 +0400

    lesson 8.5 task 16

--- clients_3_0/tests/conftest.py
+++ clients_3_0/tests/conftest.py
@@ -26,6 +26,16 @@ class UserFixture(BaseModel):
     def authentication_user(self) -> AuthenticationUserSchema:
         return AuthenticationUserSchema(email=self.email, password=self.password)
 
+# Фикстура для создания пользователя
+@pytest.fixture
+# Используем фикстуру public_users_client, которая создает нужный API клиент
+def function_user(public_users_client: PublicUsersClient) -> UserFixture:
+    create_user_api_request = CreateUserRequestSchema()
+    create_user_api_response = public_users_client.create_user_api(create_user_api_request)
+    pydantic_object_create_user_api_response = parse_api_response(CreateUserResponseSchema, create_user_api_response)
+
+    return UserFixture(request=create_user_api_request,
+                       response=pydantic_object_create_user_api_response)  # Возвращаем все нужные данные
 
 @pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
 def authentication_client() -> AuthenticationClient:  # Аннотируем возвращаемое фикстурой значение
@@ -42,17 +52,6 @@ def public_users_client() -> PublicUsersClient:  # Аннотируем возв
 def private_users_client(function_user: UserFixture) -> PrivateUsersClient:
     return get_private_users_client(function_user.authentication_user)
 
-
-# Фикстура для создания пользователя
-@pytest.fixture
-# Используем фикстуру public_users_client, которая создает нужный API клиент
-def function_user(public_users_client: PublicUsersClient) -> UserFixture:
-    create_user_api_request = CreateUserRequestSchema()
-    create_user_api_response = public_users_client.create_user_api(create_user_api_request)
-    pydantic_object_create_user_api_response = parse_api_response(CreateUserResponseSchema, create_user_api_response)
-
-    return UserFixture(request=create_user_api_request,
-                       response=pydantic_object_create_user_api_response)  # Возвращаем все нужные данные
 
 
 
